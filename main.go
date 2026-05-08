a90f177a158888639e66d0f5b5a445caa29bc884
Damoon Imani <damoonimani@pm.me>
2026-05-08 22:55:39 +0330

    changing double dash to switch to flags into a standard --flag

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
     "fmt"
     "os"
+    "strings"
 
     _ "ric/commands"     // blank import to trigger init() registration
     "ric/dispatcher"
@@ -11,18 +12,20 @@ import (
 
 func main() {
     if len(os.Args) < 2 {
-				dispatcher.Dispatch("help", nil, nil)
+        dispatcher.Dispatch("help", nil, nil)
         os.Exit(1)
     }
 
     operation := os.Args[1]
     rest := os.Args[2:]
 
+    // Split: everything before the first "--flag" is inputs,
+    // everything from that point on is flags.
     var inputs, flagArgs []string
-    for i := 0; i < len(rest); i++ {
-        if rest[i] == "--" {
+    for i, arg := range rest {
+        if strings.HasPrefix(arg, "--") {
             inputs = rest[:i]
-            flagArgs = rest[i+1:]
+            flagArgs = rest[i:]
             break
         }
     }
